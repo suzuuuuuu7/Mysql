6b9a8ea1aa620488cfcda2c1a99d4aba015dab85
suzuuuuuu7 <sujalkc324@gmail.com>
2026-01-23 18:29:06 +0545

    Fiter data from database

---
+++ Filterquery.sql
@@ -0,0 +1,6 @@
+use customers; -- use customers databases 
+select* from muju; -- select all the rows from table muju 
+select* from sujal;--  select all the column from table sujal;
+select* from sujal where id = 5; -- delete from sujal where id = 5,
+select * from sujal where age>20;-- select from table sujal where id>20 
+select* from muju where user_id>= 5; -- select from table muju user_id >=5
