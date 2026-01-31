bf4037251a10fbc8000a17d0b5cd42694f289207
suzuuuuuu7 <sujalkc324@gmail.com>
2026-01-31 16:19:59 +0545

    updated

--- union.sql
+++ union.sql
@@ -8,14 +8,15 @@
 -- ("rujal ","male","45000");
 
 select * from suzal;
-select * from sujal;
-select name from sujal union select name from suzal ;
-select id , name ,gender , salary from sujal union select id , name,gender,salary from suzal; -- union removes duplicates 
-select name from sujal where gender = "male" union select name from suzal where gender = "male";
-select name from sujal union all select name from suzal ;
-select id , name ,gender , salary from sujal union all select id , name,gender,salary from suzal; -- union all doesnot remove duplicates 
-select name from sujal where gender = "male" union all select name from suzal where gender = "male";
-select name,gender,"sujal" as role from sujal union
-select name,gender ,"suzu" as role from suzal;
-
+-- select * from sujal;
+-- select name from sujal union select name from suzal ;
+-- select id , name ,gender , salary from sujal union select id , name,gender,salary from suzal; -- union removes duplicates 
+-- select name from sujal where gender = "male" union select name from suzal where gender = "male";
+-- select name from sujal union all select name from suzal ;
+-- select id , name ,gender , salary from sujal union all select id , name,gender,salary from suzal; -- union all doesnot remove duplicates 
+-- select name from sujal where gender = "male" union all select name from suzal where gender = "male";
+-- select name,gender,"sujal" as role from sujal union
+-- select name,gender ,"suzu" as role from suzal;
+select name , gender ,salary from sujal union
+select name , gender , salary from suzal order by salary asc;
 
