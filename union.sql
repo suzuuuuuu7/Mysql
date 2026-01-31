7e058c38a8aba963f9389ef14f8bd4e6debf6d68
suzuuuuuu7 <sujalkc324@gmail.com>
2026-01-31 15:51:35 +0545

    union in sql

---
+++ union.sql
@@ -0,0 +1,10 @@
+--  create table Suzal (id int auto_increment primary key,name varchar(100) not null,gender enum("male","female","others"),salary varchar(100));
+-- insert into suzal(name,gender,salary)values
+-- ("sujal kc","male","300000"),
+-- ("tjal kc","male","56000"),
+-- ("surl kc","male","50000"),
+-- ("sujl","male","300000"),
+-- ("gujal kc","male","34000"),
+-- ("rujal ","male","45000");
+
+select * from suzal;
